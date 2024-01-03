8af6f89ec169e12861d1a2b12a14a291f9b8c6bf
riaz9191 <riazahammed9191@gmail.com>
2024-01-03 15:47:55 +0600

    As a guest, I want to create an account with GitFormed. [Backlog]

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   );
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar lg:bg-black  z-10 max-w-screen-xl lg:text-white b">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
