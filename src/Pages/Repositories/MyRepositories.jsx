cbcb64bff3d0229f1b735abd3add4ab9ab16c5aa
riaz9191 <riazahammed9191@gmail.com>
2024-01-04 13:57:44 +0600

    As a user, I want to see a list of repositories I am watching [Done]

--- src/Pages/Repositories/MyRepositories.jsx
+++ src/Pages/Repositories/MyRepositories.jsx
@@ -122,7 +122,7 @@ const MyRepositories = () => {
             className="ml-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
             onClick={sortRepositories}
           >
-            Sort
+            Sort List
           </button>
         </div>
         <ul>
